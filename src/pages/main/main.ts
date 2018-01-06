48bc99d4c0e0921aa8214090004359a1315b5645
Nick <nikos.kofotolios@thanpa.com>
2018-01-07 00:32:07 +0200

    progress

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -12,44 +12,54 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, CameraPosition, MarkerO
 })
 export class MainPage {
   map: any;
-  email: string;
 
   constructor(public navCtrl: NavController, private fire: AngularFireAuth, public platform: Platform, public locationTracker: LocationTrackerProvider, private geolocation: Geolocation, private googleMaps: GoogleMaps) {
     platform.ready().then(() => {
       let watch = this.geolocation.watchPosition();
       watch.subscribe((data) => {
-       let location = new LatLng(data.coords.latitude, data.coords.longitude);
-
-       this.map = new GoogleMap('map', {
-         'controls': {
-           'compass': true,
-           'myLocationButton': true,
-           'indoorPicker': true,
-           'zoom': true
-         },
-         'gestures': {
-           'scroll': true,
-           'tilt': true,
-           'rotate': true,
-           'zoom': true
-         },
-         'camera': {
-           'target': {
-              'lat': data.coords.latitude,
-              'lng': data.coords.longitude
-            },
-           'tilt': 30,
-           'zoom': 15,
-           'bearing': 50
-         }
-       });
-
-       this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-         console.log('Map is ready!');
-       });
+        let position = new LatLng(data.coords.latitude, data.coords.latitude);
+        this.loadMap(data.coords.latitude, data.coords.longitude);
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+          console.log('Map is ready!');
+          this.loadMarker(position);
+        });
+        
       });
+
     });
-    this.email = this.fire.auth.currentUser.email;
+  }
+
+  loadMap(latitude, longitude) {
+    this.map = new GoogleMap('map', {
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'gestures': {
+        'scroll': true,
+        'tilt': true,
+        'rotate': true,
+        'zoom': true
+      },
+      'camera': {
+        'target': {
+          'lat': latitude,
+          'lng': longitude
+        },
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+  }
+
+  loadMarker(position) {
+    this.map.addMarker({
+      title: 'Είσαι εδώ',
+      position: position
+    }).showInfoWindow();
   }
 
   start(){
